66e795a555ce7ed575deb45a4bd288fa39db123f
IvanTik <77159066+IvanTik@users.noreply.github.com>
2021-03-24 22:44:21 +0300

    Закончил проект(.1)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-
 const link = document.querySelector(".hotel-button");
 const popup = document.querySelector(".form");
-popup.classList.add("form-close");
 const map = document.querySelector(".map-img");
 map.classList.add("map-img-hide");
 
-
 link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.toggle("form-close");
+evt.preventDefault();
+if (popup.className == 'form') {
+popup.classList.remove('form');
+popup.style.display = 'block';
+popup.classList.add('form-show');
+} else {
+popup.classList.remove('form-show');
+popup.classList.add('form');
+setTimeout(() => {
+popup.style.display = 'none';
+}, 350);
+
+}
+
 });
