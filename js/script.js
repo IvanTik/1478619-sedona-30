b8791f731ed971de3d3738d449204bd7bde8b789
IvanTik <77159066+IvanTik@users.noreply.github.com>
2021-03-24 16:21:55 +0300

    Внес изменения.....

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const link = document.querySelector(".hotel-button");
 const popup = document.querySelector(".form");
 popup.classList.add("form-close");
+const map = document.querySelector(".map-img");
+map.classList.add("map-img-hide");
 
 
 link.addEventListener("click", function (evt) {
@@ -9,3 +11,7 @@ link.addEventListener("click", function (evt) {
   popup.classList.toggle("form-close");
 });
 
+// map.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   map.classList.toggle("map-img-hide");
+// });
